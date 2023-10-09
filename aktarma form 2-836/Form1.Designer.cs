0ddfc588ae9b3f9e057db4dbae40634715bfbc93
ilayda yalçın <146716704+ilaydayacin@users.noreply.github.com>
2023-10-09 20:23:47 +0300

    Add files via upload

---
+++ aktarma form 2-836/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace aktarma_form_2_836
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOde = new System.Windows.Forms.Button();
+            this.rb4 = new System.Windows.Forms.RadioButton();
+            this.rb3 = new System.Windows.Forms.RadioButton();
+            this.rb2 = new System.Windows.Forms.RadioButton();
+            this.rb1 = new System.Windows.Forms.RadioButton();
+            this.lblMiktar = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOde
+            // 
+            this.btnOde.BackColor = System.Drawing.Color.Red;
+            this.btnOde.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOde.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnOde.ForeColor = System.Drawing.Color.White;
+            this.btnOde.Location = new System.Drawing.Point(110, 214);
+            this.btnOde.Name = "btnOde";
+            this.btnOde.Size = new System.Drawing.Size(150, 110);
+            this.btnOde.TabIndex = 13;
+            this.btnOde.Text = "Öde";
+            this.btnOde.UseVisualStyleBackColor = false;
+            this.btnOde.Click += new System.EventHandler(this.btnOde_Click);
+            // 
+            // rb4
+            // 
+            this.rb4.AutoSize = true;
+            this.rb4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rb4.Location = new System.Drawing.Point(12, 169);
+            this.rb4.Name = "rb4";
+            this.rb4.Size = new System.Drawing.Size(133, 29);
+            this.rb4.TabIndex = 12;
+            this.rb4.TabStop = true;
+            this.rb4.Text = "4. Aktarma";
+            this.rb4.UseVisualStyleBackColor = true;
+            // 
+            // rb3
+            // 
+            this.rb3.AutoSize = true;
+            this.rb3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rb3.Location = new System.Drawing.Point(12, 134);
+            this.rb3.Name = "rb3";
+            this.rb3.Size = new System.Drawing.Size(133, 29);
+            this.rb3.TabIndex = 11;
+            this.rb3.TabStop = true;
+            this.rb3.Text = "3. Aktarma";
+            this.rb3.UseVisualStyleBackColor = true;
+            // 
+            // rb2
+            // 
+            this.rb2.AutoSize = true;
+            this.rb2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rb2.Location = new System.Drawing.Point(12, 99);
+            this.rb2.Name = "rb2";
+            this.rb2.Size = new System.Drawing.Size(133, 29);
+            this.rb2.TabIndex = 10;
+            this.rb2.TabStop = true;
+            this.rb2.Text = "2. Aktarma";
+            this.rb2.UseVisualStyleBackColor = true;
+            // 
+            // rb1
+            // 
+            this.rb1.AutoSize = true;
+            this.rb1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rb1.Location = new System.Drawing.Point(12, 64);
+            this.rb1.Name = "rb1";
+            this.rb1.Size = new System.Drawing.Size(139, 29);
+            this.rb1.TabIndex = 9;
+            this.rb1.TabStop = true;
+            this.rb1.Text = "1. Aktarma ";
+            this.rb1.UseVisualStyleBackColor = true;
+            // 
+            // lblMiktar
+            // 
+            this.lblMiktar.AutoSize = true;
+            this.lblMiktar.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblMiktar.Location = new System.Drawing.Point(104, 15);
+            this.lblMiktar.Name = "lblMiktar";
+            this.lblMiktar.Size = new System.Drawing.Size(59, 31);
+            this.lblMiktar.TabIndex = 8;
+            this.lblMiktar.Text = "200";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(6, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 31);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Bütçe:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 363);
+            this.Controls.Add(this.btnOde);
+            this.Controls.Add(this.rb4);
+            this.Controls.Add(this.rb3);
+            this.Controls.Add(this.rb2);
+            this.Controls.Add(this.rb1);
+            this.Controls.Add(this.lblMiktar);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOde;
+        private System.Windows.Forms.RadioButton rb4;
+        private System.Windows.Forms.RadioButton rb3;
+        private System.Windows.Forms.RadioButton rb2;
+        private System.Windows.Forms.RadioButton rb1;
+        private System.Windows.Forms.Label lblMiktar;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ aktarma form 2-836/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aktarma_form_2_836
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOde_Click(object sender, EventArgs e)
+        {
+            double sonuc = 0;
+
+
+            if (rb1.Checked)
+            {
+                sonuc = 200 - 15;
+            }
+            else if (rb2.Checked)
+            {
+                sonuc = 185 - 12.20;
+            }
+            else if (rb3.Checked)
+            {
+                sonuc = 178.8 - 8;
+            }
+
+            else if (rb4.Checked)
+            {
+                sonuc = 170.8 - 9.60;
+            }
+
+
+            lblMiktar.Text = sonuc.ToString();
+        }
+    }
+}
